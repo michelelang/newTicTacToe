2c3a255fb9b6fae2515411d3a7c29e024680380c
michelelang <michelelang3@gmail.com>
2017-12-27 10:49:00 +0200

    added click function, init function and current player

--- js/myjs.js
+++ js/myjs.js
@@ -1,1 +1,18 @@
-console.log("test");
+var ticTacToe = {};
+
+ticTacToe.init = function () {
+    ticTacToe.currentPlayer = "red";
+    ticTacToe.playerIndicator = $(".current-player-indicator");
+    ticTacToe.playerIndicator.addClass(ticTacToe.currentPlayer);
+    ticTacToe.boxes = $(".box");
+    ticTacToe.boxes.on("click", ticTacToe.boxClicked);
+};
+
+ticTacToe.boxClicked = function(){
+   $(this).addClass(ticTacToe.currentPlayer);
+};
+
+$(document).ready(function () {
+    ticTacToe.init();
+});
+
